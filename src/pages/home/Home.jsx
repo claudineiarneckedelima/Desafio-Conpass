e70cae35cf107056bb37463166a83ad25ebff026
unknown <claudinei-de-lima@hotmail.com>
2019-03-12 05:24:57 -0300

    Create pages

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import './Home.css'
+
+const Home = class App extends Component {
+    render() {
+        return (
+            <div>
+                <div id="bemvindo" className="display-6">Bem-vindo!</div>
+                <hr/>
+
+                <div className="row">
+                    <div className="col">
+                    <div className="card text-white bg-primary mb-3 text-center" style={{maxWidth: '18rem'}}>
+                        <div id="card-header1" className="card-header">Pessoas</div>
+                        <div className="card-body">
+                        <h5 id="card-title1" className="card-title">10</h5>
+                        <p className="card-text"></p>
+                        </div>
+                    </div>
+
+                    </div>
+                    <div className="col">
+                    <div className="card text-white bg-secondary mb-3 text-center" style={{maxWidth: '18rem'}}>
+                    <div id="card-header2" className="card-header">Pessoas</div>
+                    <div className="card-body">
+                        <h5 id="card-title2" className="card-title">10</h5>
+                        <p className="card-text"></p>
+                    </div>
+                    </div>
+                    </div>
+                    <div className="col">
+                    <div className="card text-white bg-danger mb-3 text-center" style={{maxWidth: '18rem'}}>
+                    <div id="card-header3" className="card-header">Pessoas</div>
+                    <div className="card-body">
+                        <h5 id="card-title3" className="card-title">10</h5>
+                        <p className="card-text"></p>
+                    </div>
+                    </div>
+                    </div>
+                    <div className="col">
+                    <div className="card text-white bg-info mb-3 text-center" style={{maxWidth: '18rem'}}>
+                    <div id="card-header4" className="card-header">Pessoas</div>
+                    <div className="card-body">
+                        <h5 id="card-title4" className="card-title">10</h5>
+                        <p className="card-text"></p>
+                    </div>
+                    </div>
+                    </div>
+
+
+
+                    <div className="col">
+                    <div className="card text-white bg-info mb-3 text-center" style={{maxWidth: '18rem'}}>
+                    <div id="card-header5" className="card-header">Pessoas</div>
+                    <div className="card-body">
+                        <h5 id="card-title5" className="card-title">10</h5>
+                        <p className="card-text"></p>
+                    </div>
+                    </div>
+                    </div><div className="col">
+                    <div className="card text-white bg-info mb-3 text-center" style={{maxWidth: '18rem'}}>
+                    <div id="card-header6" className="card-header">Pessoas</div>
+                    <div className="card-body">
+                        <h5 id="card-title6" className="card-title">10</h5>
+                        <p className="card-text"></p>
+                    </div>
+                    </div>
+                    </div><div className="col">
+                    <div className="card text-white bg-info mb-3 text-center" style={{maxWidth: '18rem'}}>
+                    <div id="card-header7" className="card-header">Pessoas</div>
+                    <div className="card-body">
+                        <h5 id="card-title7" className="card-title">10</h5>
+                        <p className="card-text"></p>
+                    </div>
+                    </div>
+                    </div><div className="col">
+                    <div className="card text-white bg-info mb-3 text-center" style={{maxWidth: '18rem'}}>
+                    <div id="card-header8" className="card-header">Pessoas</div>
+                    <div className="card-body">
+                        <h5 id="card-title8" className="card-title">10</h5>
+                        <p className="card-text"></p>
+                    </div>
+                    </div>
+                    </div><div className="col">
+                    <div className="card text-white bg-info mb-3 text-center" style={{maxWidth: '18rem'}}>
+                    <div id="card-header9" className="card-header">Pessoas</div>
+                    <div className="card-body">
+                        <h5 id="card-title9" className="card-title">10</h5>
+                        <p className="card-text"></p>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home
